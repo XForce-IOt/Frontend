fb3db3e21a67ad00c35afa89ec6b0cd057ae1313
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-04-28 15:53:20 -0500

    Add customer routing for customer module

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { CustomerPetsComponent } from './customer-pets/customer-pets.component';
+import { CustomerAppointmentsComponent } from './customer-appointments/customer-appointments.component';
+import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
+import { CustomerSettingsComponent } from './customer-settings/customer-settings.component';
 
+const routes: Routes = [
+  { path: 'customer', component: CustomerPetsComponent },
+  { path: 'customerAppointments', component: CustomerAppointmentsComponent },
+  { path: 'customerProfile/id', component: CustomerProfileComponent },
+  { path: 'customerSettings/:id', component: CustomerSettingsComponent }
+];
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class CustomerRoutingModule { }
