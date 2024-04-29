f35fdb186da4e84ca5c157117f7fde1cb57ecb71
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-04-28 20:45:02 -0500

    Update registration component and add registration module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,25 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth/auth.component';
 
-import { MatIconModule } from '@angular/material/icon';
+import { RegistrationComponent } from './registration/registration.component';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    RegistrationComponent
   ],
   imports: [
     CommonModule,
-    MatToolbarModule,
-    MatIconModule,
+    MatInputModule,
     MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatDividerModule
+    MatCardModule,
+    MatFormFieldModule
   ]
 })
 export class AuthModule { }
