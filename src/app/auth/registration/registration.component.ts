818219a53ade5f92f3b5a6becb962940cee67546
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-04-29 14:37:41 -0500

    Update styles for mat-card in auth.component.css and registration.component.html

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
+  constructor(private router: Router) { }
 
+  login() {
+    this.router.navigateByUrl('/login');
+  }
 }
