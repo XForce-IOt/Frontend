dd791b2ce13affe21dbc8ac4a36f779db3b28717
Emerson <64367045+Sheiger@users.noreply.github.com>
2024-05-02 23:08:27 -0500

    Add files via upload

---
+++ src/app/customer/customer-appointments/model/pet.entity.ts
@@ -0,0 +1,21 @@
+export class Pet {
+    id: number;
+    name: string;
+    specie: string;
+    age: string;
+    sex: string;
+    size: string;
+    perimeter: number;
+    image: string;
+  
+    constructor() {
+      this.id = 0;
+      this.name = 'none';
+      this.specie = 'none';
+      this.age = 'none';
+      this.sex = 'none';
+      this.size = 'none';
+      this.perimeter = 0;
+      this.image = 'none';
+    }
+  }
