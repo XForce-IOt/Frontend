1f8c10e653adcc27a18ca3e44c5fb229ede6ac7f
SaulMendozaB <U20211C677@upc.edu.pe>
2024-05-28 23:54:01 -0500

    auth component ts and html

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProfileService } from 'src/app/services/profile.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-auth',
@@ -8,13 +12,71 @@ import { Router } from '@angular/router';
 })
 export class AuthComponent {
 
-  constructor(private router: Router) { }
+  constructor(private profileService: ProfileService, private router: Router, private authService: AuthService) { }
 
-  login(){
-    this.router.navigateByUrl('/home/pets');
+  email = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
+
+
+  login() {
+    const emailValue = this.email.value;
+    const passwordValue = this.password.value;
+
+    if (this.email.invalid || this.password.invalid) {
+      // Al menos uno de los campos es inválido, mostrar mensajes de error
+      this.email.markAsTouched();
+      this.password.markAsTouched();
+      return;
+    }
+
+    const newItem = {
+      email: emailValue,
+      password: passwordValue
+    }
+
+    this.profileService.getList().subscribe(
+      data => {
+        const userExists = data.some(item => item.email === emailValue && item.password === passwordValue);
+
+        if (userExists) {
+
+          const user = data.find(item => item.email === emailValue && item.password === passwordValue) as User;
+    
+          this.authService.setUser(user); // Almacenar el usuario en el AuthService
+
+          this.router.navigateByUrl('/home/pets');
+        } else {
+          alert('Usuario o contraseña incorrectos');
+        }
+      },
+      error => {
+        console.log("Ocurrió un error al obtener la data");
+        console.log(error);
+      }
+    );
   }
 
+  //login(){
+  //  this.router.navigateByUrl('/home/pets');
+  //}
+
   register(){
-    this.router.navigateByUrl('/register');
+    this.router.navigateByUrl('/registration');
+  }
+
+  getErrorMessageEmail() {
+    if(this.email.hasError('required')) {
+      return 'Debes ingresar un valor';
+    }
+
+    return '';
+  }
+
+  getErrorMessagePassword() {
+    if(this.password.hasError('required')) {
+      return 'Debes ingresar un valor';
+    }
+
+    return '';
   }
 }
