bc2e0a07710799a28e4073ad6d22cdb63a56ed7d
Emerson <emersonquispe2808@gmail.com>
2024-06-08 00:43:22 -0500

    add models

---
+++ src/app/appointment-function/model/veterinarian.model.ts
@@ -0,0 +1,9 @@
+export interface Veterinarian {
+    id: Number,
+    name: string;
+    availability: string;
+    email: string;
+    phone: string;
+    photoUrl: string;
+    clinicId: number;
+}
