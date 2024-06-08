0e011a172a122bc0e357d04e3b763fd794580701
Emerson <emersonquispe2808@gmail.com>
2024-06-08 01:00:28 -0500

    update file

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:8080/api/pethealth/v1',
   googleMapsApiKey: 'AIzaSyAFtLxiBIypfr2kTVkDoop22CyQt9NEV88'
 };
