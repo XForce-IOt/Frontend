2925b6e1b89c3866686653409a9242e7359ecfe0
Emerson <emersonquispe2808@gmail.com>
2024-06-08 19:28:33 -0500

    add API backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:8080/api/pethealth/v1',
+  baseURL: 'https://backend-production-6ed3.up.railway.app/api/pet-health/v1',
   googleMapsApiKey: 'AIzaSyAFtLxiBIypfr2kTVkDoop22CyQt9NEV88'
 };
