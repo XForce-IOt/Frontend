50117f7386eb7d83e5b3bec6a0e13fb6cd93be35
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-24 10:37:32 -0500

    feat: Add access-denied component for displaying access denied message

---
+++ src/app/shared/components/access-denied/access-denied.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-access-denied',
+  templateUrl: './access-denied.component.html',
+  styleUrls: ['./access-denied.component.css']
+})
+export class AccessDeniedComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 5000); // 5000 ms = 5 segundos
+  }
+}
