bd51aa649e207ab81ea75be7dc55aa47d06e0be3
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-24 10:24:34 -0500

    chore: Add AuthGuard for route protection

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+// src/app/shared/guards/auth.guard.ts
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        const user = this.authService.getUser();
+        if (user) {
+            return true;
+        } else {
+            this.router.navigate(['/login']);
+            return false;
+        }
+    }
+}
